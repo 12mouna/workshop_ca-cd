618887f00ece40d054df5f45182deef654bafa39
12mouna <mouna.benmanaa@ensi-uma.tn>
2020-02-23 12:50:23 +0100

    circleCIDEPLOYMENT

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-const request = require('supertest'); 
+const request = require('supertest'); //jebna client mel supertest
 const server = require('./index');
 
 describe('GET Endpoints', () => {
     it('Getting the /', async () => { 
-        const res = await request(server).get('/') //cheb3eth request lel / 
+        const res = await request(server).get('/') //ab3athelna elresultat cheb3eth request lel / bel methode get
         expect(res.statusCode).toEqual(200) //ken matraja3ch erreur tjawbek b status code 200 (fel http)
     })
 })
